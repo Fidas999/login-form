c997b8c20b60a37abf53da4c41435240c10819ed
Fidas999 <fidas.drokas@gmail.com>
2019-07-19 17:32:14 +0100

    Initial Update index.js
    Testing Login Validation...
    
    Hiding Form Works good!

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 /* YOUR CODE GOES HERE! */
+function userValidation(){
+    var user = document.getElementsByTagName("input")[0].value;
+    var submit = document.getElementById("login-button");
+    console.log("form_elements: ", submit);
+}
+userValidation();
+
+
+
+
+
+
+
+
+
+function hideForm(){
+    var loginForm = document.getElementsByClassName("form");
+    var i;
+    for (i = 0; i < loginForm.length; i++) {
+        loginForm[i].style.display = 'none';
+    }
+}
