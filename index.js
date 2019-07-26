ad769bd4efdca5c31b4d97da7ea997775637ebe7
Fidas999 <fidas.drokas@gmail.com>
2019-07-26 17:23:01 +0100

    Login_Validation
    Login Validation without regex

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 /* YOUR CODE GOES HERE! */
-function userValidation(){
-    let username ="";
-    let password ="";
-    
-    document.getElementById("login-button").addEventListener("click", function(e){
-        username = document.getElementsByTagName("input")[0].value;
-        password = document.getElementsByTagName("input")[1].value;
-        alert("Username: ", username);
-         if(username.length > 3 && password.length > 7){
-             document.getElementsByClassName("form").style.display = "none"
-         }
-         else{
-             let err = document.getElementsByClassName("error")[0];
-             err.value = "Tente Novamente";
-             e.preventDefault();
-         }
-    });
-/*
-for(var i = 0; i < inputs.length; i++) {
-    if(inputs[i].type.toLowerCase() == 'text') {
-        alert(inputs[i].value);
-    }
+var username;
+var password;
+clearErrorText();
+document.getElementById("login-button").addEventListener("click", function(e){
+    username = document.getElementsByTagName("input")[0].value;
+    password = document.getElementsByTagName("input")[1].value;
+    loginValidation(e);
+});
 
+function loginValidation(e) {
+    if (username.length > 3 && password.length > 7) {
+        document.getElementsByClassName("form")[0].style.display = "none";
+        clearErrorText();
+        e.preventDefault();
+    }
+    else {
+        document.getElementsByClassName("error")[0].style.display = "initial";
+        e.preventDefault();
+    }
+}
+
+function clearErrorText(){
+    document.getElementsByClassName("error")[0].style.display = "none";
+}
+
+function regexValid(){
+
+}
+/*
     "Found " + obj[0] + " in position " + obj.index + " in the text: " + obj.input;
     console.log("form_elements: ", sub);*/
-}
-userValidation();
-
-/*function hideForm(){
-    var loginForm = document.getElementsByClassName("form");
-    var i;
-    for (i = 0; i < loginForm.length; i++) {
-        loginForm[i].style.display = 'none';
-    }
-}*/
